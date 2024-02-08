5ab9a3ea91f9a6eb1809a6b97db2f17a70408819
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-02-08 14:39:25 -0500

    Q

---
+++ example1.java
@@ -0,0 +1,14 @@
+/***************
+ * Project: Example 1 
+ * Programmer: Sebastian Richardson
+ * Date: 2/8/2024
+ ****************/
+
+public class example1{ 
+    public static void main (String[] args){
+        System.out.println("\n\n\n\n\n\n\t\t\t\t TZHZ");
+
+    }
+    
+}
+
