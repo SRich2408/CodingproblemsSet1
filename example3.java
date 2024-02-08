c07cefc2f33d9c8c2e98ab346a9690f65af88c64
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-02-08 14:53:13 -0500

    E

---
+++ example3.java
@@ -0,0 +1,13 @@
+/***************
+ * Project: Example 3
+ * Programmer: Sebastian Richardson
+ * Date: 2/8/2024
+ ****************/
+
+public class example3{ 
+    public static void main (String[] args){
+        System.out.println("\t\t\t\t\t\t\tSebastian Richardson\n\n\t\t\t\t Song: Dance with me\n\t\t\t\t Poem: I see the island still ahead somehow.");
+
+    }
+    
+}
