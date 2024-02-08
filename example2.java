0557ff2cef24070479f3ed9c42a35159172de7cd
Sebastian Richardson <sebastian.nicolasrg24@outlook.com>
2024-02-08 14:44:11 -0500

    W

---
+++ example2.java
@@ -0,0 +1,13 @@
+/***************
+ * Project: Example 2
+ * Programmer: Sebastian Richardson
+ * Date: 2/8/2024
+ ****************/
+
+public class example2{ 
+    public static void main (String[] args){
+        System.out.println("****************\n*Sue's Swimwear*\n****************");
+
+    }
+    
+}
